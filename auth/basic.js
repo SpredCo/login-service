bbc7aaa18e764dcec893cead01da14d3c032c697
Maxime Guedj <max.guedj13@gmail.com>
2016-08-23 16:03:57 +0200

    Fix

--- auth/basic.js
+++ auth/basic.js
@@ -1,11 +1,11 @@
 const passport = require('passport');
 const BasicStrategy = require('passport-http').BasicStrategy;
-const clientModel = require('spred-common').clientModel;
+const common = require('spred-common');
 
 function init () {
   passport.use(new BasicStrategy(
     function (key, secret, done) {
-      clientModel.getByCredential(key, secret, done);
+      common.clientModel.getByCredential(key, secret, done);
     }
   ));
 }
