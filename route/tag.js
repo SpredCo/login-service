11e2fd7325ae54441606ea43ce898b703f7c7832
Maxime Guedj <maxime@guedjdev.com>
2016-12-17 18:35:20 +0100

    Add get tag by name route

--- route/tag.js
+++ route/tag.js
@@ -3,6 +3,7 @@ const httpHelper = require('spred-http-helper');
 
 function registerRoute (router) {
   router.get('/tags', getAllTags);
+  router.get('/tags/:name', getTagByName);
 }
 
 function getAllTags (req, res, next) {
@@ -11,6 +12,18 @@ function getAllTags (req, res, next) {
       next(err);
     } else {
       httpHelper.sendReply(res, 200, fTags);
+    }
+  });
+}
+
+function getTagByName (req, res, next) {
+  common.tagModel.getByName(req.params.name, function (err, fTag) {
+    if (err) {
+      next(err);
+    } else if (fTag === null) {
+      httpHelper.sendReply(res, httpHelper.error.tagNotFound());
+    } else {
+      httpHelper.sendReply(res, 200, fTag);
     }
   });
 }
--- test/route/tags.js
+++ test/route/tags.js
@@ -7,7 +7,9 @@ const common = require('spred-common');
 const url = config.get('test.server.url') + ':' + config.get('test.server.port');
 const apiSrv = supertest(url);
 
-describe('Testing tags route', function () {
+var tag1;
+
+describe.only('Testing tags route', function () {
   before(function (done) {
     common.clientModel.createFix(fixture.client.name, fixture.client.key, fixture.client.secret, function (err, cClient) {
       if (err) {
@@ -21,6 +23,7 @@ describe('Testing tags route', function () {
               if (err) {
                 done(err);
               } else {
+                tag1 = cTag;
                 done();
               }
             });
@@ -41,7 +44,40 @@ describe('Testing tags route', function () {
           if (err) {
             done(err);
           } else {
-            expect(res.body).to.have.lengthOf(3);
+            expect(res.body).to.have.lengthOf(2);
+            done();
+          }
+        });
+    });
+  });
+
+  describe('Testing get tag by name (GET /v1/tags/:name)', function () {
+    it('Should return the cast information', function (done) {
+      apiSrv
+        .get('/v1/tags/' + tag1.name)
+        .set('Content-Type', 'application/json')
+        .auth(fixture.client.key, fixture.client.secret)
+        .expect(200)
+        .end(function (err, res) {
+          if (err) {
+            done(err);
+          } else {
+            expect(res.body.id).to.equal(tag1._id.toString());
+            done();
+          }
+        });
+    });
+
+    it('Should return an error if no tag is found', function (done) {
+      apiSrv
+        .get('/v1/tags/toto')
+        .set('Content-Type', 'application/json')
+        .auth(fixture.client.key, fixture.client.secret)
+        .expect(404)
+        .end(function (err, res) {
+          if (err) {
+            done(err);
+          } else {
             done();
           }
         });
