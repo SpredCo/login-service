986649d7b78e77f045f667f95ec4c8caf35985e8
Maxime Guedj <maxime@guedjdev.com>
2017-01-05 02:33:54 +0100

    Now home feed does not include private cast

--- route/feed.js
+++ route/feed.js
@@ -7,7 +7,7 @@ function registerRoute (router) {
 }
 
 function getHomeFeed (req, res, next) {
-  common.spredCastModel.find({ $or: [{ date: { $gt: new Date() }, state: 0 }, { state: 1 }] }).populate('creator tags').sort('date').exec(function (err, fCasts) {
+  common.spredCastModel.find({ $or: [{ date: { $gt: new Date() }, state: 0 }, { state: 1 }], isPublic: true }).populate('creator tags').sort('date').exec(function (err, fCasts) {
     if (err) {
       next(err);
     } else {
